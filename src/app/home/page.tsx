01d020820c8207c627302d5d53b85ec4190c34d2
saezeric <saezescalonaeric@fpllefia.com>
2025-04-11 17:09:03 +0200

    Componente header creado e insertado. Procediendo a la configuracion de este

---
+++ src/app/home/page.tsx
@@ -0,0 +1,8 @@
+// app/home/page.tsx
+export default function HomePage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Bienvenido a la página de Home</h1>
+    </div>
+  );
+}
